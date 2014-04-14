178af96f1fb27833154d4a681a6500dded6186cb
Bill Currie <bill@taniwha.org>
2014-04-14 23:21:30 +0900

    Use QF's git-version-gen.
    Rolling my own was getting too close to copying existing code, so... This
    gives me a better version number and also the dirty tag.

--- Source/git-version.sh
+++ Source/git-version.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-full_version=`git describe --tags --abbrev=4 --match="v*" | sed -e s/^v//`
+full_version=`./git-version-gen --prefix v .tarball-version`
 version=`echo $full_version | sed -e 's/-/\t/' | cut -f 1`
 
 sed -e "s/@FULL_VERSION@/$full_version/" -e "s/@VERSION@/$version/" AssemblyInfo.in > AssemblyInfo.cs-

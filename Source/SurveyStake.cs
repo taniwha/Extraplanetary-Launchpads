4a12dd4aac3b153fc67f986830a28723e303b273
Bill Currie <bill@taniwha.org>
2014-08-03 22:40:16 +0900

    Create a part module for the survey stake.

---
+++ Source/SurveyStake.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+using KSP.IO;
+
+namespace ExLP {
+
+	public class ExSurveyStake : PartModule
+	{
+		static string[] StakeUses = { "Origin",
+									  "+X", "+Y", "+Z",
+									  "-X", "-Y", "-Z"};
+		internal bool bound = false;
+		internal int use = 0;
+
+		public override string GetInfo ()
+		{
+			return "Survey Stake";
+		}
+
+		public override void OnLoad (ConfigNode node)
+		{
+		}
+
+		public override void OnStart(StartState state)
+		{
+			Events["NextUse"].guiName = StakeUses[use];
+			Events["ToggleBound"].guiName = bound ? "Bound" : "Direction";
+		}
+
+		public void FixedUpdate ()
+		{
+		}
+
+		[KSPEvent(active = true, guiActiveUnfocused = true, externalToEVAOnly = true, guiActive = false, unfocusedRange = 2f, guiName = "")]
+		public void NextUse()
+		{
+			use = (use + 1) % StakeUses.Count();
+			Events["NextUse"].guiName = StakeUses[use];
+		}
+
+		[KSPEvent(active = true, guiActiveUnfocused = true, externalToEVAOnly = true, guiActive = false, unfocusedRange = 2f, guiName = "")]
+		public void ToggleBound()
+		{
+			bound = !bound;
+			Events["ToggleBound"].guiName = bound ? "Bound" : "Direction";
+		}
+	}
+}

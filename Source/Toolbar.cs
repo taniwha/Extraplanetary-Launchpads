263651c6f74576f5c3e6e181878b93384c442d75
Bill Currie <bill@taniwha.org>
2014-10-04 07:46:59 +0900

    Use the toolbar wrapper class.
    One less dll to build and install.

--- Source/Toolbar.cs
+++ Source/Toolbar.cs
@@ -2,9 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using Toolbar;
 
 namespace ExLP {
+	using Toolbar;
+
 	[KSPAddon (KSPAddon.Startup.EditorAny, false)]
 	public class ExToolbar_ShipInfo : MonoBehaviour
 	{
--- Source/git-version.sh
+++ Source/git-version.sh
@@ -4,9 +4,7 @@ full_version=`./git-version-gen --prefix v .tarball-version`
 version=`echo $full_version | sed -e 's/-/\t/' | cut -f 1`
 
 sed -e "s/@FULL_VERSION@/$full_version/" -e "s/@VERSION@/$version/" AssemblyInfo.in > AssemblyInfo.cs-
-sed -e "s/@FULL_VERSION@/$full_version/" -e "s/@VERSION@/$version/" AssemblyInfoToolbar.in > AssemblyInfoToolbar.cs-
 
 cmp -s AssemblyInfo.cs AssemblyInfo.cs- || mv AssemblyInfo.cs- AssemblyInfo.cs
-cmp -s AssemblyInfoToolbar.cs AssemblyInfoToolbar.cs- || mv AssemblyInfoToolbar.cs- AssemblyInfoToolbar.cs
 
 rm -f *.cs-

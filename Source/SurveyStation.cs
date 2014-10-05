2758995f0bb5b6e4e3cbc4ed6f4a358beb92ade1
Bill Currie <bill@taniwha.org>
2014-10-05 17:03:17 +0900

    Transform the spawned vessel's offset.
    This fixes the launch clamps embedding themselves in the ground when
    spawned on Minmus.

--- Source/SurveyStation.cs
+++ Source/SurveyStation.cs
@@ -215,6 +215,7 @@ namespace ExLP {
 
 			Vector3 pos = ship.parts[0].transform.position;
 			Vector3 shift = new Vector3 (-pos.x, -vessel_bounds.min.y, -pos.z);
+			shift = xform.TransformDirection (shift);
 			shift += xform.position;
 			ship.parts[0].transform.Translate (shift, Space.World);
 			ship.parts[0].transform.RotateAround (xform.position,

6ee39895ffe9a903ee9ee5f5e1340b87d9e4098c
Bill Currie <bill@taniwha.org>
2015-04-06 21:29:46 +0900

    Generate an AVC version file.
    Thanks to hab136 for the info.

--- Source/git-version.sh
+++ Source/git-version.sh
@@ -8,3 +8,18 @@ sed -e "s/@FULL_VERSION@/$full_version/" -e "s/@VERSION@/$version/" AssemblyInfo
 cmp -s AssemblyInfo.cs AssemblyInfo.cs- || mv AssemblyInfo.cs- AssemblyInfo.cs
 
 rm -f *.cs-
+
+MAJOR=`echo $full_version | cut -f 1 -d .`
+MINOR=`echo $full_version | cut -f 2 -d .`
+PATCH=`echo $full_version | cut -f 3 -d .`
+BUILD=`echo $full_version | cut -f 4 -d . | cut -f 1 -d '-'`
+
+cat > EL.version <<EOF
+{
+	"NAME":"Extraplanetary Launchpads",
+	"URL":"http://taniwha.org/~bill/EL.version",
+	"DOWNLOAD":"http://forum.kerbalspaceprogram.com/threads/59545",
+	"VERSION":{"MAJOR":$MAJOR,"MINOR":$MINOR,"PATCH":$PATCH,"BUILD":$BUILD},
+	"KSP_VERSION":{"MAJOR":0,"MINOR":90,"PATCH":0}
+}
+EOF

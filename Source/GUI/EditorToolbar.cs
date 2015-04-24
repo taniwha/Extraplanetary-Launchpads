ce5bddb19b674221eef8c6bfa5fc6923f51e63c9
Bill Currie <bill@taniwha.org>
2015-04-25 02:05:11 +0900

    Set the icons for the EL modules.
    This is a bit of a hack since KSP doesn't provide an easy way to specify a
    custom module icon, but it works nicely. I now need to create some nice
    icons.

---
+++ Source/GUI/EditorToolbar.cs
@@ -0,0 +1,60 @@
+/*
+This file is part of Extraplanetary Launchpads.
+
+Extraplanetary Launchpads is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Extraplanetary Launchpads is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Extraplanetary Launchpads.  If not, see
+<http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using UnityEngine;
+using RUI.Icons.Selectable;
+
+using KSP.IO;
+
+namespace ExtraplanetaryLaunchpads {
+	[KSPAddon (KSPAddon.Startup.EditorAny, false) ]
+	public class ExEditorToolbar : MonoBehaviour
+	{
+		static Texture texture;
+		static Icon icon;
+
+		void onGUIEditorToolbarReady ()
+		{
+			var cat = PartCategorizer.Instance.filters.Where (c => c.button.categoryName == "Filter by Module").FirstOrDefault ();
+			foreach (var subcat in cat.subcategories) {
+				Debug.Log (subcat.button.categoryName);
+				if (subcat.button.categoryName.StartsWith ("EL ")) {
+					subcat.button.icon = icon;
+					subcat.button.UpdateIconState ();
+				}
+			}
+		}
+
+		void Awake ()
+		{
+			GameEvents.onGUIEditorToolbarReady.Add (onGUIEditorToolbarReady);
+			if (texture == null) {
+				texture = GameDatabase.Instance.GetTexture ("ExtraplanetaryLaunchpads/Textures/icon_button", false);
+				icon = new Icon ("EL icon", texture, texture);
+			}
+		}
+
+		void OnDestroy ()
+		{
+			GameEvents.onGUIEditorToolbarReady.Remove (onGUIEditorToolbarReady);
+		}
+	}
+}

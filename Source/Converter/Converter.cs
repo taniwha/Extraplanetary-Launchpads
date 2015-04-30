a38adaf4131ccc97d192403f5303e937b967e821
Bill Currie <bill@taniwha.org>
2015-04-30 09:31:50 +0900

    Stock ISRU integration.
    The detector integration is rather half-assed (just the magnetometer).
    
    The converters (smelter, parts converters) have been fully integrated with
    a new converter module that specifies input resources in tons/second (or
    units/second for massless resources (eg, EC)) and outputs in mass ratios.
    Required resources are supported and converted from mass to units.

---
+++ Source/Converter/Converter.cs
@@ -0,0 +1,114 @@
+/*
+This file is part of Extraplanetary Launchpads.
+
+Extraplanetary Launchpads is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Extraplanetary Launchpads is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Extraplanetary Launchpads.  If not, see
+<http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ExtraplanetaryLaunchpads {
+	public class ExConverter: BaseConverter, IModuleInfo
+	{
+		ConversionRecipe ratio_recipe;
+
+		public ConversionRecipe Recipe
+		{
+			get {
+				if (ratio_recipe == null) {
+					ratio_recipe = LoadRecipe ();
+				}
+				return ratio_recipe;
+			}
+		}
+
+		protected override float GetHeatMultiplier (ConverterResults result, double deltaTime)
+		{
+			return 1;
+		}
+
+		public override string GetInfo ()
+		{
+			return "EL Converter";
+		}
+
+		public string GetPrimaryField ()
+		{
+			return null;
+		}
+
+		public string GetModuleTitle ()
+		{
+			return "EL Converter";
+		}
+
+		public Callback<Rect> GetDrawModulePanelCallback ()
+		{
+			return null;
+		}
+
+		List<ResourceRatio> ProcessRatio (List<ResourceRatio> mass_ratios, double mass = 1)
+		{
+			var unit_ratios = new List<ResourceRatio> ();
+			for (int i = 0; i < mass_ratios.Count; i++) {
+				var ratio = new ResourceRatio ();
+				var def = PartResourceLibrary.Instance.GetDefinition (mass_ratios[i].ResourceName);
+				if (def == null) {
+					Debug.LogError (String.Format ("[EL Converter] unknown resource '{0}'", mass_ratios[i].ResourceName));
+					continue;
+				}
+				ratio.ResourceName = mass_ratios[i].ResourceName;
+				ratio.Ratio = mass * mass_ratios[i].Ratio;
+				if (def.density > 0) {
+					ratio.Ratio /= def.density;
+				}
+				ratio.DumpExcess = mass_ratios[i].DumpExcess;
+				ratio.FlowMode = mass_ratios[i].FlowMode;
+				unit_ratios.Add (ratio);
+			}
+			return unit_ratios;
+		}
+
+		double GetResourceMass (List<ResourceRatio> ratios)
+		{
+			double mass = 0;
+			for (int i = 0; i < ratios.Count; i++) {
+				var def = PartResourceLibrary.Instance.GetDefinition (ratios[i].ResourceName);
+				mass += ratios[i].Ratio * def.density;
+			}
+			return mass;
+		}
+
+		ConversionRecipe LoadRecipe ()
+		{
+			var recipe = new ConversionRecipe ();
+			recipe.Inputs.AddRange (ProcessRatio (inputList));
+			var inputMass = GetResourceMass (recipe.Inputs);
+			recipe.Outputs.AddRange (ProcessRatio (outputList, inputMass));
+			recipe.Requirements.AddRange (ProcessRatio (reqList));
+			return recipe;
+		}
+
+		protected override ConversionRecipe PrepareRecipe(double deltatime)
+		{
+			UpdateConverterStatus();
+			if (!IsActivated) {
+				return null;
+			}
+			return Recipe;
+		}
+	}
+}

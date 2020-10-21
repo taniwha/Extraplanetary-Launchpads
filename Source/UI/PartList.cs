1ceb279a61581cd22ae5551a68f8c82a1bf105f9
Bill Currie <bill@taniwha.org>
2020-10-21 12:39:01 +0900

    Clean up the mess of nested classes
    Proved to be more trouble than it was worth

---
+++ Source/UI/PartList.cs
@@ -0,0 +1,77 @@
+/*
+This file is part of Extraplanetary Launchpads.
+
+Extraplanetary Launchpads is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Extraplanetary Launchpads is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Extraplanetary Launchpads.  If not, see
+<http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
+
+using KSP.IO;
+using KSP.UI;
+using KSP.UI.Screens;
+
+using KodeUI;
+
+using ExtraplanetaryLaunchpads_KACWrapper;
+
+namespace ExtraplanetaryLaunchpads {
+
+	class ELPartList : List<AvailablePart>, UIKit.IListObject
+	{
+		ToggleGroup group;
+
+		public UnityAction<AvailablePart> onSelected { get; set; }
+		public Layout Content { get; set; }
+		public RectTransform RectTransform
+		{
+			get { return Content.rectTransform; }
+		}
+
+		public void Create (int index)
+		{
+			Content
+				.Add<ELPartItemView> ()
+					.Group (group)
+					.OnValueCanged (on => select (index, on))
+					.AvailablePart (this[index])
+					.Finish ()
+				;
+		}
+
+		public void Update (GameObject obj, int index)
+		{
+			var item = obj.GetComponent<ELPartItemView> ();
+			item.AvailablePart (this[index]);
+		}
+
+		public ELPartList (ToggleGroup group)
+		{
+			this.group = group;
+		}
+
+		void select (int index, bool on)
+		{
+			if (on) {
+				onSelected.Invoke (this[index]);
+			}
+		}
+	}
+}

e2e4ed1b75b49c71d176319664d9c3e348ef0086
Bill Currie <bill@taniwha.org>
2022-01-17 21:47:21 +0900

    Ensure craft vessel has a valid orbit
    This is in the build cost calculation only, but does take care of the
    vessel being ripped out from underneath EL while calculating the cost
    due to ObT being nan. It looks like this is the same cause for the
    breakdown when actually building a survey stake, but that might be a
    little trickier to sort out.

--- Source/BuildControl.cs
+++ Source/BuildControl.cs
@@ -1158,6 +1158,10 @@ namespace ExtraplanetaryLaunchpads {
 				lockedParts = true;
 			}
 			GameObject ro = ship.parts[0].localRoot.gameObject;
+			var od = ro.GetComponent<OrbitDriver> () ?? ro.AddComponent<OrbitDriver> ();
+			od.orbit = new Orbit(builder.vessel.orbit);
+
+			EL_Utils.dumpxform (ro.transform, true);
 			Vessel craftVessel = ro.AddComponent<Vessel>();
 			craftVessel.vesselName = "EL craftVessel - " + craft.GetValue ("ship");
 			craftVessel.Initialize (true);
